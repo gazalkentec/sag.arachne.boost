114ca64b1ae6c5ea050b6285497467623fee1c26
Александр Козулин <gazalkentec@outlook.com>
2017-02-26 00:32:03 +0700

    -

---
+++ configurator.h
@@ -0,0 +1,283 @@
+#pragma once
+
+#include <atlbase.h>
+#include <atlconv.h>
+#include "stdafx.h"
+
+#include "tinyxml/tinyxml.h"
+#pragma comment (lib, "tinyxml/tinyxml.lib")
+
+struct LoggerParameters {
+	framework::Diagnostics::LogLevel LogLevel = framework::Diagnostics::LogLevel::Info;
+	const std::string LogFileExtention = ".log";
+	std::string LogName;
+	std::string LogFilePath;
+	std::string LogFileName;
+};
+
+struct PLCParameters {
+	PLC_TYPES PLCType = PLC_TYPES::AUTODETECT;
+	int PLCPortNumber;
+	std::string PLCIPAddress;
+	int PLCPollPeriodMSec;
+};
+
+struct LocalDBParameters {
+	DB_TYPES LocalDBType;
+	std::string LocalDBPath;
+	std::string TrendsDBFileName;
+	std::string MessagesDBFileName;
+	std::string DictionariesDBFileName;
+	std::string SecretsDBFileName;
+};
+
+struct MainDBParameters {
+	std::string MainDBConnectionString;
+	int MainDBPollPeriodMSec;
+};
+
+class Configurator
+{
+private:
+
+	std::string _path;
+	SERVICE_TYPES _service_type;
+	int _service_control_port;
+	std::string _service_name;
+	const std::string _config_file_name = "service.config";
+
+	LoggerParameters _logger;
+	PLCParameters _plc;
+	LocalDBParameters _localdb;
+	MainDBParameters _maindb;
+
+	bool _is_loaded = false;
+
+public:
+
+	bool IsLoaded() { return _is_loaded; };
+	std::string ServicePath() { return _path; };
+	std::string ServiceName() { return _service_name; }
+	LPCTSTR ServiceName_LPCTSTR()
+	{
+		CA2T buff(_service_name.c_str());
+		LPCTSTR result = buff;
+		return result;
+	}
+
+	std::string LogName() { return _logger.LogName; };
+	std::string LogFile() { return _logger.LogFilePath + _logger.LogFileName; };
+	framework::Diagnostics::LogLevel LogLevel() { return _logger.LogLevel; };
+
+
+	Configurator()
+	{
+		_is_loaded = false;
+	}
+
+	bool LoadConfig(int argc, TCHAR *argv[], TCHAR *env[])
+	{
+
+		TCHAR szFileName[MAX_PATH];
+		TCHAR szPath[MAX_PATH];
+
+		GetModuleFileName(0, szFileName, MAX_PATH);
+		ExtractFilePath(szFileName, szPath);
+
+		std::wstring buff = szPath;
+
+		_path = std::string(buff.begin(), buff.end());
+
+		std::string configFile = _path + _config_file_name;
+
+		TiXmlDocument config(configFile.c_str());
+
+		if (!config.Error())
+		{
+			if (config.LoadFile(TIXML_ENCODING_UTF8))
+			{
+				try
+				{
+					TiXmlElement *service = config.FirstChildElement("service");
+
+					if (service)
+					{
+						switch (static_cast<SERVICE_TYPES>(atoi(service->Attribute("type")))) {
+
+						case SERVICE_TYPES::ARACHNE_NODE:
+							goto EXIT;
+
+							break;
+						case SERVICE_TYPES::ARACHNE_PLC_CONTROL:
+
+							_service_name = service->Attribute("name");
+
+							if (!_service_name.empty())
+							{
+								_service_type = SERVICE_TYPES::ARACHNE_PLC_CONTROL;
+								_service_control_port = atoi(service->Attribute("command_port"));
+
+								TiXmlElement *logger = service->FirstChildElement("log");
+								if (logger)
+								{
+									_logger.LogName = _service_name;
+
+									std::string buff = logger->Attribute("alter_file_name");
+									if (!buff.empty()) {
+										_logger.LogFileName = buff;
+									}
+									else _logger.LogFileName = _logger.LogName + _logger.LogFileExtention;
+
+									buff.clear();
+									buff = logger->Attribute("alter_path");
+									if (!buff.empty()) {
+										if (buff.substr(0, 2) == ".\\") {
+											_logger.LogFilePath = _path + buff.substr(2, buff.length());
+										}
+										else _logger.LogFilePath = buff;
+									}
+									else _logger.LogFilePath = _path;
+
+									_logger.LogLevel = static_cast<framework::Diagnostics::LogLevel>(atoi(logger->Attribute("level")));
+
+									TiXmlElement *plc = service->FirstChildElement("plc");
+									if (plc)
+									{
+										_plc.PLCType = static_cast<PLC_TYPES>(atoi(plc->Attribute("type")));
+										_plc.PLCPollPeriodMSec = atoi(plc->Attribute("poll_period_msecv"));
+										_plc.PLCPortNumber = atoi(plc->Attribute("port"));
+										_plc.PLCIPAddress = plc->Attribute("ip_address");
+
+										TiXmlElement *localdb = service->FirstChildElement("localdb");
+										if (localdb)
+										{
+
+											_localdb.LocalDBType = static_cast<DB_TYPES>(atoi(localdb->Attribute("type")));
+
+											switch (static_cast<DB_TYPES>(atoi(localdb->Attribute("type")))) {
+
+											case DB_TYPES::DB_ORACLE:
+												goto EXIT;
+
+												break;
+
+											case DB_TYPES::DB_MSSQL:
+												goto EXIT;
+
+												break;
+
+											case DB_TYPES::DB_SQLITE:
+
+												buff.clear();
+												buff = localdb->Attribute("alter_path");
+												if (!buff.empty()) {
+													if (buff.substr(0, 2) == ".\\") {
+														_localdb.LocalDBPath = _path + buff.substr(2, buff.length());
+													}
+													else _localdb.LocalDBPath = buff;
+												}
+												else _localdb.LocalDBPath = _path;
+
+												_localdb.DictionariesDBFileName = _localdb.LocalDBPath + localdb->Attribute("dictionaries");
+												_localdb.MessagesDBFileName = _localdb.LocalDBPath + localdb->Attribute("messages");
+												_localdb.SecretsDBFileName = _localdb.LocalDBPath + localdb->Attribute("secrets");
+												_localdb.TrendsDBFileName = _localdb.LocalDBPath + localdb->Attribute("trends");
+
+												break;
+											default:
+												goto EXIT;
+											}
+
+											TiXmlElement *maindb = service->FirstChildElement("maindb");
+											if (maindb)
+											{
+												switch (static_cast<DB_TYPES>(atoi(maindb->Attribute("type")))) {
+
+												case DB_TYPES::DB_ORACLE:
+
+													_maindb.MainDBConnectionString = maindb->Attribute("conn_string");
+													_maindb.MainDBPollPeriodMSec = atoi(maindb->Attribute("poll_period_msec"));
+
+													break;
+
+												case DB_TYPES::DB_MSSQL:
+													goto EXIT;
+
+													break;
+
+												case DB_TYPES::DB_SQLITE:
+													goto EXIT;
+
+													break;
+												default:
+													goto EXIT;
+												}
+
+												_is_loaded = true;
+												return _is_loaded;
+											}
+											else
+											{
+												goto EXIT;
+											}
+										}
+										else
+										{
+											goto EXIT;
+										}
+									}
+									else
+									{
+										goto EXIT;
+									}
+								}
+								else
+								{
+									goto EXIT;
+								}
+							}
+							else
+							{
+								goto EXIT;
+							}
+
+							break;
+						case SERVICE_TYPES::ARACHNE_WEIGHT_CONTROL:
+							goto EXIT;
+
+							break;
+						case SERVICE_TYPES::ARACHNE_TERMAL_CONTROL:
+							goto EXIT;
+
+							break;
+						case SERVICE_TYPES::ARACHNE_BUZZER_CONTROL:
+							goto EXIT;
+
+							break;
+						default:
+							goto EXIT;
+						}
+
+					}
+					else goto EXIT;
+				}
+				catch (...)
+				{
+					goto EXIT;
+				}
+			}
+		}
+
+	EXIT:
+		_is_loaded = false;
+		return _is_loaded;
+
+		//WRITELOG(logger, framework::Diagnostics::LogLevel::Info, _T("Config is cuccessfully loaded... try to parse..."));
+		//WRITELOG(logger, framework::Diagnostics::LogLevel::Info, LPWSTR(config.Value()));
+	}
+
+	~Configurator()
+	{
+	}
+};
+
--- main.cpp
+++ main.cpp
@@ -1,6 +1,337 @@
-#include <stdio.h>
+//**********************************************************//
+//															//
+//	sag.osiris.plc.exchange									//
+//															//
+//**********************************************************//
 
-int main()
+#include <Windows.h>
+#include <tchar.h>
+#include <atlbase.h>
+#include <atlconv.h>
+
+#include "stdafx.h"
+#include "configurator.h"
+
+using namespace std;
+using namespace framework::Diagnostics;
+using namespace framework::Threading;
+
+//#define  SERVICE_NAME  _T("sag.osiris.plc.exchange")
+
+Configurator cnf;
+
+SERVICE_STATUS			g_ServiceStatus = { 0 };
+SERVICE_STATUS_HANDLE	g_StatusHandle = NULL;
+HANDLE					g_ServiceStopEvent = INVALID_HANDLE_VALUE;
+
+VOID WINAPI ServiceMain(DWORD argc, LPTSTR *argv);
+
+VOID WINAPI ServiceCtrlHandler(DWORD);
+
+DWORD WINAPI ServiceWorkerThread(LPVOID lpParam);
+DWORD WINAPI PLCExchangeWorker(LPVOID lpParam);
+DWORD WINAPI MAINDBExchangeWorker(LPVOID lpParam);
+
+int _tmain(int argc, TCHAR *argv[], TCHAR *env[])
 {
-	return -1;
+
+	/*
+	//for (int i = 0; i < 10; i++)
+	//{
+	//	std::cout << "\a" << std::endl;
+
+	//	Sleep(1000);
+	//}
+
+	//std::cout << "Parameters count: " << argc << std::endl;
+
+	//std::cout << "Parameters: " << std::endl;
+
+	//for (int i = 0; argv[i]; i++)
+	//{
+
+	//	std::wstring buff = argv[i];
+	//
+	//	std::cout << std::string(buff.begin(), buff.end()) << std::endl;
+
+	//}
+
+	//std::cout << "Environment: " << std::endl;
+
+	//for (int i = 0; env[i]; i++)
+	//{
+
+	//	std::wstring buff = env[i];
+
+	//	std::cout << std::string(buff.begin(), buff.end()) << std::endl;
+
+	//std::cout << char(*env[i]);
+	//
+	//for (; *env[i]++; )
+	//{
+	//	std::cout << char(*env[i]);
+	//}
+
+	//std::cout << std::endl;
+	//}
+
+	//system("PAUSE >> VOID");
+
+	//return(EXIT_SUCCESS); */
+
+	cnf.LoadConfig(argc, argv, env);
+
+	if (!cnf.IsLoaded())
+	{
+		return -1;
+	}
+
+	CA2T buff(cnf.ServiceName().c_str());
+	LPCTSTR tamburine = buff;
+
+	CLogger<CNoLock> logger(cnf.LogLevel(), tamburine);
+
+	logger.AddOutputStream(new std::wofstream(cnf.LogFile()), true, cnf.LogLevel());
+
+	WRITELOG(logger, framework::Diagnostics::LogLevel::Info, _T("Config is loaded successfully. Server is try to start..."));
+
+
+
+	SERVICE_TABLE_ENTRY ServiceTable[] =
+	{
+		{ LPWSTR(cnf.ServiceName().c_str()), (LPSERVICE_MAIN_FUNCTION)ServiceMain },
+		{ NULL, NULL }
+	};
+
+	if (StartServiceCtrlDispatcher(ServiceTable) == FALSE)
+	{
+		WRITELOG(logger, framework::Diagnostics::LogLevel::Error, _T("Server start error! Shutdown!"));
+		return GetLastError();
+	}
+
+	WRITELOG(logger, framework::Diagnostics::LogLevel::Info, _T("Server is stopped..."));
+
+	return ERROR_SUCCESS;
+}
+
+
+VOID WINAPI ServiceMain(DWORD argc, LPTSTR *argv)
+{
+	CA2T buff(cnf.ServiceName().c_str());
+	LPCTSTR tamburine = buff;
+
+	CLogger<CNoLock> logger(cnf.LogLevel(), tamburine);
+
+	logger.AddOutputStream(new std::wofstream(cnf.LogFile()), true, cnf.LogLevel());
+
+	DWORD Status = E_FAIL;
+
+	g_StatusHandle = RegisterServiceCtrlHandler(LPWSTR(cnf.ServiceName().c_str()), ServiceCtrlHandler);
+
+	if (g_StatusHandle == NULL)
+	{
+		WRITELOG(logger, framework::Diagnostics::LogLevel::Error, _T("Server start error! Is will be stopped!"));
+		goto EXIT;
+	}
+
+	// Tell the service controller we are starting
+	ZeroMemory(&g_ServiceStatus, sizeof(g_ServiceStatus));
+	g_ServiceStatus.dwServiceType = SERVICE_WIN32_OWN_PROCESS;
+	g_ServiceStatus.dwControlsAccepted = 0;
+	g_ServiceStatus.dwCurrentState = SERVICE_START_PENDING;
+	g_ServiceStatus.dwWin32ExitCode = 0;
+	g_ServiceStatus.dwServiceSpecificExitCode = 0;
+	g_ServiceStatus.dwCheckPoint = 0;
+
+	if (SetServiceStatus(g_StatusHandle, &g_ServiceStatus) == FALSE)
+	{
+		//OutputDebugString(_T("My Sample Service: ServiceMain: SetServiceStatus returned error"));
+	}
+
+	/*
+	* Perform tasks neccesary to start the service here
+	*/
+	//OutputDebugString(_T("My Sample Service: ServiceMain: Performing Service Start Operations"));
+
+	// Create stop event to wait on later.
+	g_ServiceStopEvent = CreateEvent(NULL, TRUE, FALSE, NULL);
+	if (g_ServiceStopEvent == NULL)
+	{
+		//OutputDebugString(_T("My Sample Service: ServiceMain: CreateEvent(g_ServiceStopEvent) returned error"));
+
+		g_ServiceStatus.dwControlsAccepted = 0;
+		g_ServiceStatus.dwCurrentState = SERVICE_STOPPED;
+		g_ServiceStatus.dwWin32ExitCode = GetLastError();
+		g_ServiceStatus.dwCheckPoint = 1;
+
+		if (SetServiceStatus(g_StatusHandle, &g_ServiceStatus) == FALSE)
+		{
+			//OutputDebugString(_T("My Sample Service: ServiceMain: SetServiceStatus returned error"));
+		}
+		goto EXIT;
+	}
+
+	// Tell the service controller we are started
+	g_ServiceStatus.dwControlsAccepted = SERVICE_ACCEPT_STOP;
+	g_ServiceStatus.dwCurrentState = SERVICE_RUNNING;
+	g_ServiceStatus.dwWin32ExitCode = 0;
+	g_ServiceStatus.dwCheckPoint = 0;
+
+	if (SetServiceStatus(g_StatusHandle, &g_ServiceStatus) == FALSE)
+	{
+		//OutputDebugString(_T("My Sample Service: ServiceMain: SetServiceStatus returned error"));
+	}
+
+	// Start the thread that will perform the main task of the service
+	HANDLE hThread = CreateThread(NULL, 0, ServiceWorkerThread, NULL, 0, NULL);
+
+	// Wait until our worker thread exits effectively signaling that the service needs to stop
+	WaitForSingleObject(hThread, INFINITE);
+
+
+	/*
+	* Perform any cleanup tasks
+	*/
+
+	CloseHandle(g_ServiceStopEvent);
+
+	g_ServiceStatus.dwControlsAccepted = 0;
+	g_ServiceStatus.dwCurrentState = SERVICE_STOPPED;
+	g_ServiceStatus.dwWin32ExitCode = 0;
+	g_ServiceStatus.dwCheckPoint = 3;
+
+	if (SetServiceStatus(g_StatusHandle, &g_ServiceStatus) == FALSE)
+	{
+		//OutputDebugString(_T("My Sample Service: ServiceMain: SetServiceStatus returned error"));
+	}
+
+EXIT:
+
+	return;
+}
+
+
+VOID WINAPI ServiceCtrlHandler(DWORD CtrlCode)
+{
+	CA2T buff(cnf.ServiceName().c_str());
+	LPCTSTR tamburine = buff;
+
+	CLogger<CNoLock> logger(cnf.LogLevel(), tamburine);
+
+	logger.AddOutputStream(new std::wofstream(cnf.LogFile()), true, cnf.LogLevel());
+
+	//OutputDebugString(_T("My Sample Service: ServiceCtrlHandler: Entry"));
+
+	switch (CtrlCode)
+	{
+	case SERVICE_CONTROL_STOP:
+
+		WRITELOG(logger, framework::Diagnostics::LogLevel::Info, _T("Server stop request. Service will be stopped..."));
+
+		if (g_ServiceStatus.dwCurrentState != SERVICE_RUNNING)
+			break;
+
+		/*
+		* Perform tasks neccesary to stop the service here
+		*/
+
+		g_ServiceStatus.dwControlsAccepted = 0;
+		g_ServiceStatus.dwCurrentState = SERVICE_STOP_PENDING;
+		g_ServiceStatus.dwWin32ExitCode = 0;
+		g_ServiceStatus.dwCheckPoint = 4;
+
+		if (SetServiceStatus(g_StatusHandle, &g_ServiceStatus) == FALSE)
+		{
+			//OutputDebugString(_T("My Sample Service: ServiceCtrlHandler: SetServiceStatus returned error"));
+		}
+
+		// This will signal the worker thread to start shutting down
+		SetEvent(g_ServiceStopEvent);
+
+		break;
+
+	default:
+		break;
+	}
+
+	//OutputDebugString(_T("My Sample Service: ServiceCtrlHandler: Exit"));
+}
+
+
+DWORD WINAPI ServiceWorkerThread(LPVOID lpParam)
+{
+	CA2T buff(cnf.ServiceName().c_str());
+	LPCTSTR tamburine = buff;
+
+	CLogger<CNoLock> logger(cnf.LogLevel(), tamburine);
+
+	logger.AddOutputStream(new std::wofstream(cnf.LogFile()), true, cnf.LogLevel());
+
+	WRITELOG(logger, framework::Diagnostics::LogLevel::Info, _T("main worker is running..."));
+
+	// Start the thread that will perform the main task of the service
+	CreateThread(NULL, 0, MAINDBExchangeWorker, NULL, 0, NULL);
+	Sleep(300);
+	// Start the thread that will perform the main task of the service
+	CreateThread(NULL, 0, PLCExchangeWorker, NULL, 0, NULL);
+	Sleep(300);
+	//  Periodically check if the service has been requested to stop
+	while (WaitForSingleObject(g_ServiceStopEvent, 0) != WAIT_OBJECT_0)
+	{
+
+
+		//  Simulate some work by sleeping
+		Sleep(550);
+	}
+
+	WRITELOG(logger, framework::Diagnostics::LogLevel::Info, _T("main worker exit..."));
+
+	return ERROR_SUCCESS;
+}
+
+
+DWORD WINAPI PLCExchangeWorker(LPVOID lpParam)
+{
+	CA2T buff(cnf.ServiceName().c_str());
+	LPCTSTR tamburine = buff;
+
+	CLogger<CNoLock> logger(cnf.LogLevel(), tamburine);
+
+	logger.AddOutputStream(new std::wofstream(cnf.LogFile()), true, cnf.LogLevel());
+
+	WRITELOG(logger, framework::Diagnostics::LogLevel::Info, _T("PLC exchange worker is running..."));
+
+	while (WaitForSingleObject(g_ServiceStopEvent, 0) != WAIT_OBJECT_0)
+	{
+
+		//  Simulate some work by sleeping
+		Sleep(200);
+	}
+
+	WRITELOG(logger, framework::Diagnostics::LogLevel::Info, _T("PLC exchange worker exit..."));
+
+	return ERROR_SUCCESS;
+}
+
+DWORD WINAPI MAINDBExchangeWorker(LPVOID lpParam)
+{
+	CA2T buff(cnf.ServiceName().c_str());
+	LPCTSTR tamburine = buff;
+
+	CLogger<CNoLock> logger(cnf.LogLevel(), tamburine);
+
+	logger.AddOutputStream(new std::wofstream(cnf.LogFile()), true, cnf.LogLevel());
+
+	WRITELOG(logger, framework::Diagnostics::LogLevel::Info, _T("MainDB worker is running..."));
+
+	while (WaitForSingleObject(g_ServiceStopEvent, 0) != WAIT_OBJECT_0)
+	{
+
+		//  Simulate some work by sleeping
+		Sleep(1000);
+	}
+
+	WRITELOG(logger, framework::Diagnostics::LogLevel::Info, _T("MainDB worker exit..."));
+
+	return ERROR_SUCCESS;
 }
